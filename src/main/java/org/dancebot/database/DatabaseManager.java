245984785e1ce507fb9aef3ad50d133ffbdd5d1a
= <=>
2023-12-17 16:37:59 +0500

    new database, changing coaches added

--- src/main/java/org/dancebot/database/DatabaseManager.java
+++ src/main/java/org/dancebot/database/DatabaseManager.java
@@ -10,7 +10,7 @@ public class DatabaseManager {
     public void insertClient(int tg_id) {
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Соединение с базой данных установлено");
+            System.out.println("Connected successfully");
 
             String sql = "INSERT INTO tg_ids (tg_id) VALUES (?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -18,83 +18,77 @@ public class DatabaseManager {
 
             int rowsAffected = preparedStatement.executeUpdate();
             if (rowsAffected > 0) {
-                System.out.println("Новая запись добавлена успешно");
+                System.out.println("New row is added successfully");
             } else {
-                System.out.println("Ошибка при добавлении новой записи");
+                System.out.println("New row error");
             }
 
             connection.close();
-            System.out.println("Соединение с базой данных закрыто");
+            System.out.println("Connection is closed");
         } catch (SQLException e) {
-            System.out.println("Ошибка при работе с базой данных: " + e.getMessage());
+            System.out.println("Database error: " + e.getMessage());
         }
     }
-    public void insertCoach_tgId(int tg_id) {
+    public void insertCoach(int tg_id, String nickname, String info, String timing) {
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Соединение с базой данных установлено");
+            System.out.println("Connected successfully");
 
-            String sql = "INSERT INTO coaches (tg_id) VALUES (?)";
+            String sql = "INSERT INTO coaches (tg_id, nickname, info, timing) VALUES (?,?,?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, tg_id);
+            preparedStatement.setString(2, nickname);
+            preparedStatement.setString(3, info);
+            preparedStatement.setString(4, timing);
 
             int rowsAffected = preparedStatement.executeUpdate();
             if (rowsAffected > 0) {
-                System.out.println("Новая запись добавлена успешно");
+                System.out.println("New row is added successfully");
             } else {
-                System.out.println("Ошибка при добавлении новой записи");
+                System.out.println("New row error");
             }
 
             connection.close();
-            System.out.println("Соединение с базой данных закрыто");
+            System.out.println("Connection is closed");
         } catch (SQLException e) {
-            System.out.println("Ошибка при работе с базой данных: " + e.getMessage());
+            System.out.println("Database error: " + e.getMessage());
         }
     }
-    public void insertCoach_nickname(String nickname) {
-        try {
+    public void change_nickname(String new_name, String old_name){
+        try{
+        Connection connection = DriverManager.getConnection(url, username, password);
+        System.out.println("Connected successfully");
+        String sql = "UPDATE coaches SET nickname = '?' WHERE nickname = '?';" ;
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, new_name);
+        preparedStatement.setString(2,old_name);
+        int rowsUpdated = preparedStatement.executeUpdate();
+        if (rowsUpdated==1){
+            System.out.println("A row is updated successfully");
+        }else{
+            System.out.println("New row error");
+        }
+        } catch (SQLException e) {
+        System.out.println("Database error: " + e.getMessage());
+        }
+    }
+
+    public void change_timing(String new_timing, String name){
+        try{
             Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Соединение с базой данных установлено");
-
-            String sql = "INSERT INTO coaches (nickname) VALUES (?)";
+            System.out.println("Connected successfully");
+            String sql = "UPDATE coaches SET info = '?' WHERE nickname = '?';" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, nickname);
-
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Новая запись добавлена успешно");
-            } else {
-                System.out.println("Ошибка при добавлении новой записи");
+            preparedStatement.setString(1, new_timing);
+            preparedStatement.setString(2, name);
+            int rowsUpdated = preparedStatement.executeUpdate();
+            if (rowsUpdated==1){
+                System.out.println("A row is updated successfully");
+            }else{
+                System.out.println("New row error");
             }
-
-            connection.close();
-            System.out.println("Соединение с базой данных закрыто");
         } catch (SQLException e) {
-            System.out.println("Ошибка при работе с базой данных: " + e.getMessage());
+            System.out.println("Database error: " + e.getMessage());
         }
     }
-    public void insertCoach_info(String info) {
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Соединение с базой данных установлено");
-
-            String sql = "INSERT INTO coaches (tg_id) VALUES (?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, info);
-
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Новая запись добавлена успешно");
-            } else {
-                System.out.println("Ошибка при добавлении новой записи");
-            }
-
-            connection.close();
-            System.out.println("Соединение с базой данных закрыто");
-        } catch (SQLException e) {
-            System.out.println("Ошибка при работе с базой данных: " + e.getMessage());
-        }
-    }
-
-
 }
