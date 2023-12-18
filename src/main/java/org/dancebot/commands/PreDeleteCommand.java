354676af679d83310f64b6a16f9f76a31a83fc97
feerrrum <139925598+feerrrum@users.noreply.github.com>
2023-12-18 19:36:04 +0500

    Update PreDeleteCommand.java

--- src/main/java/org/dancebot/commands/PreDeleteCommand.java
+++ src/main/java/org/dancebot/commands/PreDeleteCommand.java
@@ -15,11 +15,11 @@ public class PreDeleteCommand implements TextCommand{
 
     @Override
     public boolean canBeApply(UserSession session, String text) {
-        return session.getState().equals(UserState.ON_RECORD) && "Удалить".equals(text);
+        return session.getState().equals(UserState.ON_RECORD) && "РЈРґР°Р»РёС‚СЊ".equals(text);
     }
 
     @Override
     public CommandResult execute(UserSession session, String text) throws SQLException {
-        return new CommandResult("Кого?", dbHandler.getNicks());
+        return new CommandResult("РљРѕРіРѕ?", dbHandler.getNicks());
     }
 }
